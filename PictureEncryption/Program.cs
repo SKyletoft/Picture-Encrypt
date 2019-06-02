93c6d7a9d9c780c63075e065507700693d8ccce4
Samuel Kyletoft <skyletoft@gmail.com>
2019-06-02 20:35:54 +0200

    Initial commit

---
+++ PictureEncryption/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+
+class Program
+{
+    static void Main()
+    {
+        var input = Console.ReadLine().Split(' ');
+        var longestWord = 0;
+        for (var i = 0; i < input.Length; i++)
+        {
+            longestWord = Math.Max(longestWord, input[i].Length);
+        }
+        var image = new Bitmap(longestWord, input.Length);
+        for (var i = 0; i < input.Length; i++)
+        {
+            for (var j = 0; j < input[i].Length; j++)
+            {
+                image.SetPixel(j, i, Color.FromArgb(255, (int)input[i][j], (int)input[i][j], (int)input[i][j]));
+            }
+        }
+        image.Save("outputImage.png");
+    }
+}
---
+++ PictureEncryptionOpen/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+
+class Program
+{
+    static void Main()
+    {
+        var image = new Bitmap("0xF1L3.png");
+        for (var i = 0; i < image.Height; i++)
+        {
+            for (var j = 0; j < image.Width; j++)
+            {
+                Console.Write((char)(image.GetPixel(j, i).R));
+            }
+            Console.WriteLine();
+        }
+    }
+}
